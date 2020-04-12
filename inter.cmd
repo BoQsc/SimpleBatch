54cbbcfdbab1a4eb4185431969fe3b4b9a6ac612
Feldwor <BoQsc@users.noreply.github.com>
2020-04-12 19:43:09 +0300

    Update inter.cmd

--- inter.cmd
+++ inter.cmd
@@ -5,10 +5,13 @@
 ::bitsadmin /transfer myDownloadJob /download /priority normal https://api.ipify.org "%cd%asd.txt"
 
 SET "newDownload=%RANDOM%-inter"
+echo %newDownload%
 bitsadmin /create %newDownload%
 echo %ERRORLEVEL%
-bitsadmin /addfile %newDownload% "https://api.ipify.org" "%cd%/asd.txt"
+bitsadmin /addfile %newDownload% "https://api.ipify.org" "%cd%\asd.txt"
 echo %ERRORLEVEL%
+
+bitsadmin /sethttpmethod %newDownload% get
 bitsadmin /resume %newDownload% 
 echo %ERRORLEVEL%
 
@@ -17,16 +20,21 @@ echo err count:
 bitsadmin /RawReturn /geterrorcount "1472-inter" 
 bitsadmin /getstate %newDownload%
 echo %ERRORLEVEL%
+
+
+timeout "5"
+
 bitsadmin /geterror %newDownload%
 echo %ERRORLEVEL%
+
 
 
  
 bitsadmin /setnotifycmdline <job> exit
 
-https://gist.github.com/WiVi71/b0766dd658b50df5b873f676e3d128bd
-https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/bitsadmin-setnotifycmdline
-https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/bitsadmin-setnotifyflags
+::https://gist.github.com/WiVi71/b0766dd658b50df5b873f676e3d128bd
+::https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/bitsadmin-setnotifycmdline
+::https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/bitsadmin-setnotifyflags
 
 
 
